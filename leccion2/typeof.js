57222cd7f4e70f085af1f4bcbf0c902e41acaa97
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-27 16:46:08 -0300

    typeof exercise

---
+++ leccion2/typeof.js
@@ -0,0 +1,25 @@
+
+//las variable son dinamicas ya que podemos resignificarla y su data type no es statica ni se asigna al realizar el codigo
+
+
+// tipos de dato string
+var nombre="Amanecer"
+
+console.log(typeof nombre);
+
+//tipos de dato numerico
+var numero = 100
+console.log(typeof numero);
+
+nombre = 10;
+console.log(typeof nombre);
+
+
+//tipo de dato object
+
+var object ={
+    nombre : "Ami",
+    apellido : "Cabrera",
+    hijos : "1"
+}
+console.log(typeof object);
