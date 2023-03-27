35c856e5ed70e6baab6643c9abe77801ad1ba046
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-27 17:35:44 -0300

    array and empty string

---
+++ leccion2/array-empty-string.js
@@ -0,0 +1,43 @@
+
+//array es un tipo object y se identifican por tener [corchetes]
+var autos = ['BMW', 'Audi', 'Volvo'];
+console.log(autos)
+
+console.log(typeof autos)
+
+//cadena vacia empty string
+
+var z = '';
+console.log(z)
+
+console.log(z)//empty string
+
+console.log(typeof z)//strinf
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
