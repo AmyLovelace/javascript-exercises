a5dd3ec0e3e3ed6caffd942b61e5c65d61d93856
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-27 17:00:54 -0300

    data types

---
+++ leccion2/booleandatatype.js
@@ -0,0 +1,39 @@
+//boolean datatype (true, false)
+
+var bandera = true;
+console.log(bandera);
+console.log(typeof bandera);
+
+//datatype function 
+
+function mifuncion (){
+
+}
+console.log(mifuncion)
+console.log(typeof mifuncion);
+
+//datatype symbol
+
+var simbolo = Symbol("mi simbolo");
+console.log(Symbol)
+console.log (typeof Symbol)
+
+// datatype clase 
+class inay{
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+console.log(inay)
+console.log (typeof inay)
+
+//datatype undefined
+var x;
+console.log (x)
+console.log (typeof x)
+
+//null ausencia de valor 
+var y = null;
+console.log (y)
+console.log (typeof y)
