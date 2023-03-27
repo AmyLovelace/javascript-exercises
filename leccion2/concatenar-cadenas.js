1d5ceb43e333fc6f6923ae506c5c1f2dcd5f53ef
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-27 17:45:43 -0300

    concatenacion strings

---
+++ leccion2/concatenar-cadenas.js
@@ -0,0 +1,20 @@
+var nombre = 'Ami';
+var apellido = 'Cabrera';
+
+var nombreCompleto = nombre + ' ' + apellido;
+console.log(nombreCompleto);
+
+var nombreCompleto2 ='Adel'+ ' ' + 'Cabrera';
+console.log(nombreCompleto2);
+
+var x = nombre + 666;
+console.log(x);
+
+var x = nombre + (6+ 6+ 6);
+console.log(x);
+
+x = 2 + 5 + nombre;
+console.log(x);
+console.log(typeof x);//string
+
+
