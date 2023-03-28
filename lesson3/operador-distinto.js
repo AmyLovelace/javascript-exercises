731a9c7f4e05d9af883cf47130158300a50855e7
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-28 20:20:10 -0300

    operador distinto a

---
+++ lesson3/operador-distinto.js
@@ -0,0 +1,11 @@
+let a = 3 , b= 2 , c = "3";
+
+//Boolean
+let z = a != c;
+console.log(z)// revisa el valor sin importar el tipo
+//false
+
+z = a !== c; //revisa los valores pero tambien los tipos de data
+console.log(z)
+//true
+
