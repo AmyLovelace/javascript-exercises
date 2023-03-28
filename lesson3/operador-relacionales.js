e3ffb05571f81e70682a962f78bd2814d93643c2
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-28 20:34:11 -0300

    operador relacional menor y mayo o igual

---
+++ lesson3/operador-relacionales.js
@@ -0,0 +1,22 @@
+let a = 3 , b= 3 , c = "3";
+
+//boolean 
+let z = a < b;//menor
+console.log(z)
+//false
+
+z = a <= b;//menor o igual
+console.log(z)
+//true
+
+z = a > b;//mayor
+console.log(z)
+//false
+
+z = a >= b;//mayor o igual
+console.log(z)
+//true
+
+
+
+
