9fed6eee073421b506cb5e1a7b763f3b6259f776
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-28 20:01:41 -0300

    operadores de asignacion

---
+++ lesson3/operadores-de-asignacion.js
@@ -0,0 +1,22 @@
+let a = 1 ;
+
+a += 3;//4
+
+// a = a + 3 operador de asignador compuesto
+a -= 2;/2
+ // a = a - 2
+
+
+
+ // otros operadores :
+ /*
+
+ *= multiplicacion 
+ /= division
+ %= modulo operador 
+ **= potencia
+
+ */
+
+
+
