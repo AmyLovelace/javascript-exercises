728055e85fb398050d1c30811a0d767026557359
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-28 20:14:48 -0300

    operadores de comparacion

---
+++ lesson3/operadores-de-comparacion.js
@@ -0,0 +1,10 @@
+let a = 3 , b= 2 , c = "3";
+
+//Boolean
+let z = a == c;
+console.log(z)// revisa el valor sin importar el tipo en este caso convirtio string a int
+//true
+
+z = a === c; //revisa los valores pero tambien los tipos de data
+console.log(z)
+//false
