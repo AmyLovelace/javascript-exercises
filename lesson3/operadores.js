bdd65df73e8438e10365791f552cf5fb242339a9
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-28 18:38:03 -0300

    operadores ex

---
+++ lesson3/operadores.js
@@ -0,0 +1,20 @@
+// utilizamos la palabra VAR y recomiendan utilizar la palabra reservada LET Y CONST (variable que no se puede re asignar valor)
+
+let a =3; b =2;
+let z = a + b ;
+console.log("resultado de la suma : " + z);
+
+z = a - b;
+console.log("resultado de la resta: " + z);
+
+z = a * b;
+console.log("resultado de la multiplicacion: " + z);
+
+z = a / b;
+console.log("resultado de la division : " + z)
+
+z = a % b;//residuo de la division
+console.log("resultado de la operacion modulo: " + z)
+
+z = a ** b;
+console.log("resultado de la operacion potencia: " + z)
