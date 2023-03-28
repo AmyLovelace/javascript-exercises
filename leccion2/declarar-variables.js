dfa71ec2753b9c26402beda3b73c67b41f2b85c1
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-28 18:10:16 -0300

    declarar variables

--- leccion2/declarar-variables.js
+++ leccion2/declarar-variables.js
@@ -1,4 +1,33 @@
 // utilizamos la palabra VAR y recomiendan utilizar la palabra reservada LET Y CONST (variable que no se puede re asignar valor)
 
-nombre = "juan";
-console.log(typeof nombre)
+let nombre = "juan";
+
+console.log(nombre)
+
+const apellido ="Cabrera";
+console.log(apellido)
+//apellido = "Perez" ERROR NO SE PUEDE DAR OTRO VALOR A UNA VARIABLE CONST
+
+//let nombreCompleto = "Amanecer Cabrera"; //para nombrar correctamente hay que ocupar este metodo la segunda palabra tiene mayus en su primera letra
+//console.log(nombreCompleto);
+
+let x, y;
+x=10, y = 20;
+let z = x + y ;
+console.log(z); //30 
+//las variables son sensibles a mayus y minus y no pueden tener un numero en su primer caracter
+
+let nombreCompleto = "Amanecer Cabrera";
+let nombrecompleto = "Andres Avila";
+console.log(nombreCompleto);
+console.log(nombrecompleto);
+
+//casos incorrectos
+//let 1nombrecompleto;no utilizar numeros como primer caracter 
+//let break = 10; no utilizar palabras reservadas para nombres de variables 
+
+
+
+
+
+
