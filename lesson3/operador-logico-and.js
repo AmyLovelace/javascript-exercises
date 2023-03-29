01ede8addba1a336c1667a561ebf38191a5f711d
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-28 21:25:36 -0300

    operador logico

---
+++ lesson3/operador-logico-and.js
@@ -0,0 +1,11 @@
+// && = and 
+
+let a = 11;
+let valorMinimo = 0, valorMaximo = 10;
+
+if (a >= valorMinimo && a <= valorMaximo){
+    console.log("dentro del rango");
+}
+else{
+    console.log("fuera de rango");
+}
