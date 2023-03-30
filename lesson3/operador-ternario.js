15ab0c90c487d81afeb8aa49c1ff832aa6d83e4b
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-30 17:30:22 -0300

    operador ternario

---
+++ lesson3/operador-ternario.js
@@ -0,0 +1,12 @@
+//condicion si la condicion es true se ejecuta la expresion 1 , si la expresion es falsa se ejecuta expresion falsa
+
+/*let resultado =(1>2)? "verdadero" : 0;
+console.log(resultado);*/
+
+/*let numero = 11;
+resultado =( numero % 2 == 0)? "Número par": "Número impar";
+console.log(resultado);*/
+
+let impar =200 ; 
+resultado = (impar % 2 == 1)?  "Número impar": "Número par";
+console.log(resultado);
