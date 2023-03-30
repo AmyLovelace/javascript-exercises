350d1d7fa0f12aaea1f046d632eb14b9fb1f8aeb
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-30 20:10:21 -0300

    precedencia operadores

---
+++ lesson3/precedenciaoperadores.js
@@ -0,0 +1,13 @@
+let x = 5;
+
+let y = 10;
+let z = ++x + y--;
+console.log(x);//6
+console.log(y);//9
+console.log(z);//16
+
+let resultado = 4 + 5 * 6 / 3;
+console.log(resultado);//14
+
+resultado = (4+5) *6/3;
+console.log(resultado);
