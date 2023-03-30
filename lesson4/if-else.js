0f671392f4b4647ba851bf96d67f581d50401e84
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-30 20:57:41 -0300

    if else

--- lesson4/if-else.js
+++ lesson4/if-else.js
@@ -1,6 +1,6 @@
 let condicion = true
 
-if (condicion){
-    console.log("condicion verdadera");
-}
-else{console.log("condicion falsa");}
+if (condicion)
+    {console.log("condicion verdadera");}
+
+else {console.log("condicion falsa");}
