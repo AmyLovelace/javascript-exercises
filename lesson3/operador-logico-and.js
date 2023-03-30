3b5f5e9ac2c3c0629a3208d936d87430ce6f8d11
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-30 17:05:41 -0300

    and operator

--- lesson3/operador-logico-and.js
+++ lesson3/operador-logico-and.js
@@ -1,5 +1,4 @@
-// && = and 
-
+// && = and regresa true si ambos son true
 let a = 15;
 let valorMinimo = 0, valorMaximo = 10;
 
@@ -8,4 +7,13 @@ if (a >= valorMinimo && a <= valorMaximo){
 }
 else{
     console.log("fuera de rango");
+}
+
+//ejemplo de or(||) regresa true si uno de los operandos sea true
+let vacaciones = true; diaDescanso = false;
+if(vacaciones|| diaDescanso){
+    console.log("puede asistir al juego del hijo");
+}
+else{
+    console.log("padre esta ocupado");
 }
