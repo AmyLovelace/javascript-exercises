09d02619595263af0c6ddd40621f9d75cec91182
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-30 19:37:23 -0300

    string to number

---
+++ lesson3/convertir-string-number.js
@@ -0,0 +1,14 @@
+let miNumero = "18";
+
+//console.log(typeof miNumero);
+
+let edad = Number (miNumero);
+/*console.log( edad);
+if (edad >=18){
+    console.log("puedes votar");
+}
+else{
+    console.log("no puedes votar");
+}*/
+mayorDeedad =(edad >=18)? "puedes votar":"no puedes votar";
+console.log(mayorDeedad)
