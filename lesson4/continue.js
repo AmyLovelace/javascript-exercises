04641e6d85aec3ccd3688c2d01ca3ba69db70dfc
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-02 22:27:28 -0400

    D

---
+++ lesson4/continue.js
@@ -0,0 +1,16 @@
+for(let contador = 0; contador <= 10; contador++){
+    if (contador %2 !==0 ){
+        continue;//ir a la siguiente iteracion
+    }
+    
+        console.log(contador)
+    
+}
+
+
+
+
+
+
+
+
