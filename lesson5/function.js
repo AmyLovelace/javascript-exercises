c79ed81d67d12affab979340ec65231a496576ac
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-04 19:04:29 -0400

    return js

---
+++ lesson5/function.js
@@ -0,0 +1,25 @@
+//declaracion de la funcion
+function misMichitos (a,b){
+    console.log("suma: " + (a + b));
+
+}
+misMichitos(1,2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lesson5/return.js
@@ -0,0 +1,15 @@
+//declaracion de la funcion
+function misMichitos (a,b){
+    return a + b;
+
+}
+
+//lamado de la funcion con ( y los argumentos,argumentos)
+let resultado = misMichitos(1,2);
+console.log(resultado)
+
+//declaracion funcion de tipo expresion
+let x =function(a,b){return a + b}
+
+resultado =x(1 , 2)
+console.log(resultado)
