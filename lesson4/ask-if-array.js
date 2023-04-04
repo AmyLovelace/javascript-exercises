a436ea476b542f722fa97a9ed1435d70a41def57
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-04 18:36:50 -0400

    ask array

---
+++ lesson4/ask-if-array.js
@@ -0,0 +1,22 @@
+
+const michis = ['petusa','pancito','molotov','mechita','copin']
+
+console.log(michis[3])
+
+michis[2] ='katiuska'; //asi modificas un array
+
+michis.push('walter');//se agrega un elemento al final del array
+
+console.log(michis.length)
+michis[michis.length]='comebasula'//asi puedes agregar al final del array
+
+for (let i = 0 ; i< michis.length; i ++){
+    console.log(i + ' : ' + michis[i] + ' cosita preciosa')
+}
+
+console.log(typeof michis)
+
+console.log(Array.isArray(michis))
+
+console.log(michis instanceof Array);
+
