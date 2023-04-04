3f61f7c46d3359e71a9033a002848344e66633cf
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-04 18:20:28 -0400

    array elements ex

---
+++ lesson4/elements-arrays-exi.js
@@ -0,0 +1,24 @@
+
+const michis = ['petusa','pancito','molotov','mechita','copin']
+
+console.log(michis[3])
+
+for (let i = 0 ; i< michis.length; i ++){
+    console.log(i + ' : ' + michis[i] + ' cosita preciosa')
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lesson4/elements-arrays.js
@@ -0,0 +1,27 @@
+
+const autos = ['BMW','Mercedes Benz','Volvo'];
+console.log(autos);
+
+
+for (let i = 0; i < autos.length; i++){
+    console.log(i + ' : ' + autos[i])
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
