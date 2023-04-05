cf2ca2409b3ca972c1989306562affbf63584538
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-05 16:12:31 -0400

    arrow function

---
+++ lesson5/arrow-function.js
@@ -0,0 +1,11 @@
+let sumar =function(a,b){return a + b};
+resultado = sumar(1,2);
+console.log(resultado);
+
+
+//el operador flecha => define el cuerpo de nuestra funcion
+
+const sumararrowfunction = (a , b) => a + b;
+//guardamos la funcion en una variable y les entregamos los parametros 
+resultado= sumararrowfunction(3,5);
+console.log(resultado);
