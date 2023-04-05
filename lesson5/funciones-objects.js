1357ebf9edfd080208a0f70928927e078279776f
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-05 16:05:12 -0400

    function objects

--- lesson5/funciones-objects.js
+++ lesson5/funciones-objects.js
@@ -1,9 +1,26 @@
+//las funciones pueden ser descritas como objetos
+
+//declaracion de la funcion
+function misMichitos (a,b){
+   console.log(arguments.length)
+    return a + b;
+}
+
+//lamado de la funcion con ( y los argumentos,argumentos)
+let resultado = misMichitos(1,2);
+console.log(resultado)
+
+//declaracion funcion de tipo expresion
+let x =function(a,b){return a + b}
+
+resultado =x(1 , 2)
+console.log(resultado)
 
 
 
+var misMichitostexto = misMichitos.toString();
 
-
-
+console.log(misMichitostexto)
 
 
 
