df10992b9ebed1a4a39d49b47922b5896bfef0be
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-06 19:25:22 -0400

    objects

---
+++ lesson6/objects.js
@@ -0,0 +1,28 @@
+
+let persona = {
+    nombre : 'Ami',
+    apellido: 'Lovelace',
+    email:'AmyLovelace@gmail.com',
+    edad:28
+}
+
+console.log(persona.apellido)
+console.log(persona.nombre)
+console.log(persona.email)
+console.log(persona.edad)
+
+console.log(persona)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
