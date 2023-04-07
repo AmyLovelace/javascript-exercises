e0b9406f214b77bc75ca92b4a59c6f1b74c5df1a
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-06 20:13:00 -0400

    creating objects

--- lesson6/creating-objects.js
+++ lesson6/creating-objects.js
@@ -1,3 +1,24 @@
+let persona = {
+    nombre : 'Ami',
+    apellido: 'Lovelace',
+    email:'AmyLovelace@gmail.com',
+    edad:28,
+    nombreCompleto: function(){
+        return this.nombre +' ' + this.apellido;
+
+    }
+}
+
+console.log(persona.apellido)
+console.log(persona.nombre)
+
+console.log(persona)
+console.log(persona.nombreCompleto())
+
+let persona2 = new Object();
+persona2.nombre = 'Adel';
+persona2.apellido = 'Cabrera';
+console.log(persona2)
 
 
 
