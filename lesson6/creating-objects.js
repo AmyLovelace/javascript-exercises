70b9797288f3f173957c9a5ca2a59359f6221d75
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-06 20:14:30 -0400

    creating objects

--- lesson6/creating-objects.js
+++ lesson6/creating-objects.js
@@ -18,7 +18,10 @@ console.log(persona.nombreCompleto())
 let persona2 = new Object();
 persona2.nombre = 'Adel';
 persona2.apellido = 'Cabrera';
+persona2.tel = '22223333'
 console.log(persona2)
+
+
 
 
 
