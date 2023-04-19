d2585f90062083d57adcdb0e33be136a26a59721
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-18 20:21:22 -0400

    create and delete objs

---
+++ lesson6/create-and-delete-objects.js
@@ -0,0 +1,14 @@
+let persona = { 
+    nombre : 'Ami',
+    apellido: 'Lovelace',
+    email:'AmyLovelace@gmail.com',
+    edad:28,
+    nombreCompleto: function(){
+        return this.nombre +' ' + this.apellido;
+
+    }
+}
+
+persona.tel ='55555555'
+persona.tel ='55558555'
+console.log(persona)
