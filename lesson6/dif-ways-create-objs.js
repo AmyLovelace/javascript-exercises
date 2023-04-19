849e142cf35d0874077496ee529db9b200dcaee4
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-19 18:04:41 -0400

    def ways of creating objs

---
+++ lesson6/dif-ways-create-objs.js
@@ -0,0 +1,22 @@
+function michis(nombre,apellido,status){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.nombreCompleto = function(){
+        return this.nombre + ' ' + this.apellido;
+    }
+    this.status = status;
+
+}
+
+let katiuska = new michis('katiuska', 'molotov', 'dissapear');
+console.log(katiuska.nombreCompleto(),katiuska)
+
+let copito = new michis ('copito', 'armando','8 lifes left');
+console.log(copito.status)
+
+
+
+let miObjeto = new Object();
+let miObjeto2 = 'hola';
+
+console.log(miObjeto2)
