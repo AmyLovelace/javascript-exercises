d2d13340727c44853acb3aae50eba01c8e17e9aa
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-18 20:28:36 -0400

    dif prints objs

---
+++ lesson6/prints.js
@@ -0,0 +1,13 @@
+let persona = { 
+    nombre : 'Ami',
+    apellido: 'Lovelace',
+    email:'AmyLovelace@gmail.com',
+    edad:28,
+    nombreCompleto: function(){
+        return this.nombre +' ' + this.apellido;
+
+    }
+}
+
+persona.tel ='55555555';
+persona.tel ='55558555';
