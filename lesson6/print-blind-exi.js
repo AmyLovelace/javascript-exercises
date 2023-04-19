12aaab61d69d75f0a44990ee4ef561f82cc64103
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-18 20:47:03 -0400

    blind exercise

---
+++ lesson6/print-blind-exi.js
@@ -0,0 +1,10 @@
+let michito ={
+    nombre : 'katiuska',
+    apellido : 'molotov',
+    direccion : 'las encinas 930',
+    status : 'dead',
+}
+
+for(propiedadMichito in michito){
+    console.log(michito[propiedadMichito])
+}
