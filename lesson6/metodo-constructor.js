88eca260c337a166d12591fbd4ab4f657a764c6b
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-19 15:27:02 -0400

    agregar metodos a un metodo constructor de objectos

--- lesson6/metodo-constructor.js
+++ lesson6/metodo-constructor.js
@@ -3,12 +3,17 @@ function bichito(nombre , apellido,email){
     this.nombre = nombre;
     this.apellido = apellido;
     this.email = email;
+    this.nombreCompleto = function(){
+        return this.nombre + ' ' + this.apellido;
+    }
 
 }
 
 let insecto = new bichito('bichicio','tarantulo', 'btaran@gmail.com');
-console.log(insecto);
+console.log(insecto.nombreCompleto());
 
 
 let cucaracha = new bichito ('rachel', 'roach', 'rroach@gmail.com');
-console.log(cucaracha);
+console.log(cucaracha.nombreCompleto());
+
+//asi podemos reutilizar codigo para crear objetos con los parametros que necesitemos 
