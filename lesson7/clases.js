98b614dcde55dd3bcc0e8dfc8b2327cc74b497ec
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-20 11:14:26 -0400

    added class exercise

---
+++ lesson7/clases.js
@@ -0,0 +1,13 @@
+class Persona {
+    constructor(nombre,apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+
+    }
+}
+
+let persona1 = new Persona('Ami','Cabrera');//instancia de clase u objeto
+console.log(persona1);
+
+let persona2= new Persona('Andy', 'Avila');
+console.log(persona2);
