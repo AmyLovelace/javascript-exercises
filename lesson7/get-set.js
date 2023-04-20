5b253deac0dd4cd92c516f6f1a9782ec01d0108e
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-20 12:10:05 -0400

    added get set exercise

---
+++ lesson7/get-set.js
@@ -0,0 +1,19 @@
+class michi{
+    constructor(nombre,apellido){
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+    get nombre(){
+        return this._nombre;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+}
+
+let micha = new michi('Katiuska','Molotov');
+micha.nombre = 'chechi'// set nombre 
+
+console.log(micha.nombre)//metodo get nombre
+
