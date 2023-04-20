e8da0db891d9e1046b62f3e19f5321739b956e27
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-20 11:18:19 -0400

    added class exercise

---
+++ lesson7/clases-blind.js
@@ -0,0 +1,9 @@
+class michi{
+    constructor(nombre,apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+let micha = new michi('Katiuska','Molotov');
+console.log(micha)
