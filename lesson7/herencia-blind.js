88c91ccb92c4c9de696152c4ffb18f6f2564ba80
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-20 18:50:20 -0400

    added a herencia ex

--- lesson7/herencia-blind.js
+++ lesson7/herencia-blind.js
@@ -19,6 +19,16 @@ class Michi{
     set apellido(apellido){
         this._apellido = apellido;
     }
+
+    nombreCompleto(){
+        return this._nombre +' ' + this._apellido;
+    }
+//aobreescribiendo el metodo de la clase padre 
+    toString(){
+        //se aplica polimorfismo (multiples formas en tiempo de ejecucion)
+        //el metodo que se ejecuta depende si es una referencia de tipo padre o hijo
+        return this.nombreCompleto();
+    }
 }
 
 class Status extends Michi{
@@ -34,8 +44,14 @@ class Status extends Michi{
     set status(status){
         this._status = status;
     }
+
+    //sobreescritura
+    nombreCompleto(){
+        return super.nombreCompleto()+', ' + this._status;
+    }
 }
 
 let Estado = new Status ('Katiuska', 'Molotov','desaparecida')
-console.log(Estado)
+console.log(Estado.nombreCompleto())
  
+console.log(Estado.toString())
