29e90b68aca837d59e8632d5c3973421708b0c83
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-25 19:50:04 -0400

    strict mode

---
+++ lesson7/strict-mode.js
@@ -0,0 +1,13 @@
+"use strict"// debe ser la primera linea de nuestro programa o dentro de una funcion 
+
+let x = 10;
+
+console.log(x);
+
+function miFuncion (){
+    "use strict"//y is not defined 
+    y = 10;
+} 
+
+console.log(miFuncion());
+
