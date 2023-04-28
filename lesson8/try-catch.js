d84fca22d9f7bad48290083b5daf46f0c7a8d6fe
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-28 19:07:54 -0400

    added an try catch ex

--- lesson8/try-catch.js
+++ lesson8/try-catch.js
@@ -5,7 +5,8 @@
 try{
     
     //x = 10;
-    mifuncion();
+    //mifuncion();
+    let x = 10; //sin error se salta CATCH y se ejecuta FINALLY
 }
 //utilizamos el catch para "capturar"el error y lo utilizamos en el parametro de la variable catch 
 //la mandamos a imprimir , nos devuelve el error y logramos que siga leyendo el programa. 
@@ -14,7 +15,7 @@ catch(error){
     console.log(error);
 
 }
-//podemos agregar el bloque FINALLY que siempre se ejecutara independiente que haya dentro del TRY
+//podemos agregar el bloque FINALLY(opcional) que siempre se ejecutara independiente que haya dentro del TRY
 finally{
     console.log('termina la revisión de errores');
 }
