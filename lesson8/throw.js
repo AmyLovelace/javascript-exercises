e4a36a37199837fb9cf4a55da2cb467ebd69e36d
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-28 19:55:06 -0400

    added an try catch ex

---
+++ lesson8/throw.js
@@ -0,0 +1,23 @@
+'use strict'
+
+let resultado = '';
+
+try{
+    //x = 10;
+
+    if(isNaN(resultado))throw 'no es un numero';
+    else if(resultado === '') throw 'es una cadena vacia';
+    else if (resultado >= 0) throw 'valor positivo';
+    else if (resultado < 0) throw 'valor negativo';
+
+}
+catch(error){
+    console.log(error);
+    //console.log(error.name);
+    //console.log(error.message);
+
+}
+finally{
+    console.log('termina revision de errores');
+}
+
