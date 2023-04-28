e7d6ddf331f06acf0ad60c15e3a365a634b9fecf
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-28 19:04:17 -0400

    added an try catch ex

--- lesson8/try-catch.js
+++ lesson8/try-catch.js
@@ -1,4 +1,22 @@
-//ejercicio try catch
+'use strict'
+//ejercicio try catch manejo de errores
+
+//envolver la expresion con error en un bloque try-catch
+try{
+    
+    //x = 10;
+    mifuncion()
+}
+//utilizamos el catch para "capturar"el error y lo utilizamos en el parametro de la variable catch 
+//la mandamos a imprimir , nos devuelve el error y logramos que siga leyendo el programa. 
+
+catch(error){
+    console.log(error);
+
+}
+
+console.log('continuamos..')
+
 
 
 
