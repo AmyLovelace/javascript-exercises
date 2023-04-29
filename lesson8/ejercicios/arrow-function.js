89667c38f460cf907b0ed9e8e6d4dd4f4d409ff5
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-28 21:20:26 -0400

    added an arrow function ex

--- lesson8/ejercicios/arrow-function.js
+++ lesson8/ejercicios/arrow-function.js
@@ -1,4 +1,8 @@
 // hacer una arrow function "filtrarPares" que filtra los numeros pares ocupando el operador .filter toma "numeros "como parametro de la funcion
 
-const filtrarPares = (numeros) => numeros.filter(numero => numero % 2 === 0);//operador de estricta igualdad ( === )
+//const filtrarPares = (numeros) => numeros.filter(numero => numero % 2 === 0);//operador de estricta igualdad ( === )
+//console.log(filtrarPares([1,2,3,4,5,6,7,8,9,10,11,12,13]));
+
+
+const filtrarPares = (numeros) => numeros.filter(numero => numero % 2 === 0 && numero % 3 === 0);//operador de estricta igualdad ( === )
 console.log(filtrarPares([1,2,3,4,5,6,7,8,9,10,11,12,13]));
