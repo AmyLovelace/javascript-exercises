fbdff716c4179dd2f53bc928a1feb4796c843946
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-29 21:16:58 -0400

    added a setInterval ex

--- lesson8/setInterval.js
+++ lesson8/setInterval.js
@@ -1,3 +1,13 @@
+// setinterval manda a llamar la funcion callback cada  ciertos segundos a diferencia de la settimeout 
+
+let reloj = () => {
+    let fecha = new Date ();
+    console.log (`${fecha.getHours()}:${fecha.getMinutes()}:${fecha.getSeconds()}`);
+}
+
+setInterval(reloj,1000);//cada un segundo
+
+
 
 
 
