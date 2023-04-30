c2d836b2217cdbe689bffcfa8e05326c149355ab
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-29 21:55:37 -0400

    added a promises and setTimeout ex

--- lesson9/setTimeout-promises.js
+++ lesson9/setTimeout-promises.js
@@ -1,3 +1,21 @@
+//declarar variable
+//let miPromesa = new Promise((resolved,rejected) => {//la promesa recibe dos parametros ambos funciones callback la primera es la que queremos ejecutar y la segunda es una funcion para ejecutar en caso de fallo
+//    let expresion = false; 
+//    if (expresion)
+//        resolved('Resolvió correcto');
+//    else
+//       rejected('se produjo un error');
+//});
 
+//miPromesa.then(valor => console.log(valor), error => console.log(error));
+//miPromesa.then(valor => console.log(valor)).catch(error => console.log(error));
 
+let promesa = new Promise((resolved) => {
+    console.log('inicio promesa');
+    setTimeout(() => resolved ('saludos con promesa y timeout'),3000);
+    console.log('fin promesa');
+});
+
+//declaramos nuestra promesa sin utilizar catch solo then
+promesa.then(valor => console.log(valor));
 
