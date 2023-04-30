e3bf7e6111b32d91ebf959485d7e237d9d84b4d0
amanecer <amanecer.luz.cabrera@gmail.com>
2023-04-29 22:13:14 -0400

    added a promises async await and setTimeout ex

--- lesson9/Async-Await-setTimeout.js
+++ lesson9/Async-Await-setTimeout.js
@@ -1,5 +1,16 @@
 //promesa,await ,async y setTimeout
 
+async function funcionConPromesaAwaityTimeout(){
+    console.log('inicio de la funcion');
+    let miPromesa = new Promise(resolved => {
+        setTimeout(() => resolved ('promesa con await y timeout'), 3000);
+    });
+    console.log(await miPromesa);
+    console.log('fin de la funcion');
+}
+
+funcionConPromesaAwaityTimeout();
+
 
 
 
